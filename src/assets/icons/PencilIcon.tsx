dd67898bc2fb95b7602f3c68ab6ed336daeb6905
daniellarikov16 <hopperlarikov@gmail.com>
2025-04-08 23:12:58 +0300

    modified:   src/assets/icons/PencilIcon.tsx

--- src/assets/icons/PencilIcon.tsx
+++ src/assets/icons/PencilIcon.tsx
@@ -1,10 +1,10 @@
 import { FC } from 'react';
-import { IconProps } from '../../types/commonTypes'
+import { IconProps } from '../../types/commonTypes';
 
 export const PencilIcon: FC<IconProps> = props => {   
-    const{
+    const {
         className,
-        color = '#313130',
+        color = '#313131',
         height = 28,
         width = 28,
         onClick
@@ -19,18 +19,34 @@ export const PencilIcon: FC<IconProps> = props => {
             viewBox="0 0 32 32" 
             xmlns="http://www.w3.org/2000/svg"
             fill={color}
-            >
-            <path d="M75,30 L85,40 L60,65 L50,55 L75,30 Z" stroke={color} stroke-width="2"/>
+        >
+            <path 
+                d="M22 8L26 12L14 24L10 20L22 8Z" 
+                stroke={color} 
+                strokeWidth="2" 
+                fill={color}
+            />
             
-            <path d="M25,50 L50,25 L55,30 L30,55 L25,50 Z" fill="#D1A05A" stroke="#B88C4A" stroke-width="2"/>
+            <path 
+                d="M10 20L14 24L8 30L4 26L10 20Z" 
+                fill="#D1A05A" 
+                stroke="#B88C4A" 
+                strokeWidth="2"
+            />
             
-            <path d="M50,25 L55,30 L25,50 L20,45 L50,25 Z" fill="#C0C0C0" stroke="#A0A0A0" stroke-width="2"/>
+            <path 
+                d="M14 12L18 16L10 24L6 20L14 12Z" 
+                fill="#C0C0C0" 
+                stroke="#A0A0A0" 
+                strokeWidth="2"
+            />
             
-            <path d="M20,45 L25,50 L15,60 L10,55 L20,45 Z" fill="#555555" stroke="#333333" stroke-width="2"/>
-            
-            <path d="M30,40 L35,45" stroke="#B88C4A" stroke-width="2" stroke-linecap="round"/>
-            <path d="M35,35 L40,40" stroke="#B88C4A" stroke-width="2" stroke-linecap="round"/>
-            <path d="M40,30 L45,35" stroke="#B88C4A" stroke-width="2" stroke-linecap="round"/>
+            <path 
+                d="M4 26L8 30L2 32L0 30L4 26Z" 
+                fill="#555555" 
+                stroke="#333333" 
+                strokeWidth="2"
+            />
         </svg>
     );
 }
