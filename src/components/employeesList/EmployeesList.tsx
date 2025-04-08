f4f2aa108d6e47aa7291b5f5e34541207a36aee6
daniellarikov16 <hopperlarikov@gmail.com>
2025-04-08 22:15:23 +0300

    modified:   src/components/employeesList/EmployeesList.tsx 	modified:   src/components/employeesList/EmployeesListProps.ts 	new file:   src/components/employeesList/employeesListStyles.scss 	modified:   src/pages/department/departmentsPageStyles.scss

--- src/components/employeesList/EmployeesList.tsx
+++ src/components/employeesList/EmployeesList.tsx
@@ -1,12 +1,15 @@
 import { FC, useState } from 'react';
 import { EmployeesListProps } from './EmployeesListProps';
+import clsx from 'classnames';
+import './employeesListStyles.scss'
 
 export const EmployeesList: FC<EmployeesListProps> = props => {
-    const {employeesList} = props;
+    const {employeesList, onItemClick} = props;
     const [selectedUser, setSelectedUser] = useState(0);
 
     const employeeClickHandler = (id: number) => {
         setSelectedUser(id);
+        onItemClick && onItemClick(id);
     }
 
     const isSelected = (id: number) => selectedUser === id;
@@ -15,7 +18,9 @@ export const EmployeesList: FC<EmployeesListProps> = props => {
         <div>
             {employeesList.map(user => {
                 return (
-                    <div key = {user.id} onClick={() => employeeClickHandler(user.id)}>
+                    <div key = {user.id} 
+                        className={clsx('empl-list__item', {'empl-list__item_selected': isSelected(user.id)})}
+                        onClick={() => employeeClickHandler(user.id)}>
                         {`${user.lastName} ${user.firstName} ${user.middleName ?? ''}`.trim()}
                     </div>)
             })}
