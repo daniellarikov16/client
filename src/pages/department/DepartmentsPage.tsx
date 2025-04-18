125f6a4592c587bb593d185b791ab6429c0846bb
daniellarikov16 <hopperlarikov@gmail.com>
2025-04-18 17:33:07 +0300

    modified:   src/pages/department/DepartmentsPage.tsx 	modified:   src/types/models.ts

--- src/pages/department/DepartmentsPage.tsx
+++ src/pages/department/DepartmentsPage.tsx
@@ -4,20 +4,29 @@ import './departmentsPageStyles.scss'
 import { Dialog, DropDown, TextField } from "../../components";
 import { EmployeesList } from "../../components";
 import { Button } from "../../components";
-import { Employee } from '../../types/models'
+import { Employee, Department } from '../../types/models'
+import { DropDownItem } from "../../components/dropDown/DropDownProps";
 
 const fakeEmployeesData = [
-    {id: 1, lastName: 'Иванов', firstName: 'Иван', middleName:  'Иванович'},
-    {id: 2, lastName: 'Петров', firstName: 'Сергей', middleName:  'Дмитриевич'},
-    {id: 3, lastName: 'Степанов', firstName: 'Степан' },
-    {id: 4, lastName: 'Олегов', firstName: 'Олег', middleName:  'Олегович'},
-    {id: 5, lastName: 'Сергеев', firstName: 'Сергей', middleName:  'Сергеевич'}
-];
+    {id: 1, lastName: 'Иванов', firstName: 'Иван', middleName:  'Иванович', birthDate: new Date(). toISOString(), email: "ivan@mail.ru", phoneNumber: '8-800-888-88-88'},
+    {id: 2, lastName: 'Петров', firstName: 'Сергей', middleName:  'Дмитриевич',  birthDate: new Date(). toISOString(), email: "ivan@mail.ru", phoneNumber: '8-800-888-88-88'},
+    {id: 3, lastName: 'Степанов', firstName: 'Степан',  birthDate: new Date(). toISOString(), email: "ivan@mail.ru", phoneNumber: '8-800-888-88-88' },
+    {id: 4, lastName: 'Олегов', firstName: 'Олег', middleName:  'Олегович',  birthDate: new Date(). toISOString(), email: "ivan@mail.ru", phoneNumber: '8-800-888-88-88'},
+    {id: 5, lastName: 'Сергеев', firstName: 'Сергей', middleName:  'Сергеевич',  birthDate: new Date(). toISOString(), email: "ivan@mail.ru", phoneNumber: '8-800-888-88-88'}
+] as Array<Employee>;
 
+const fakeDepartmentsData = [
+    {id: 1, name: "Отдел 1", employees: []},
+    {id: 2, name: "Отдел 2", employees: fakeEmployeesData},
+    {id: 3, name: "Отдел 3", employees: []}
+] as Array<Department>;
 
 export const DepartmentsPage: FC = () => {
-    const [employeesData, setEmployeesData] = useState<Array<Employee>>([])
-    const[showEmployeeDialog, setShowEmployeeDialog] = useState (false);
+    const [employeesData, setEmployeesData] = useState<Array<Employee>>([]);
+    const [departmentsData, setDepartmentsData] = useState<Array<Department>>([]);
+    const [selectedDepartmentId, setSelectedDepartmentId] = useState <number>();
+    const [selectedEmloyeeId, setSelectedEmployeeId] = useState<number>();
+    const [showEmployeeDialog, setShowEmployeeDialog] = useState (false);
     const [UserActionMode, setUserActionMode] = useState<'create' | 'edit'>('create');
     const [UserToEdit, setUserToEdit] = useState(0);
     
@@ -29,10 +38,23 @@ export const DepartmentsPage: FC = () => {
         setEmployeesData(fakeEmployeesData);
     }, []);
 
+    useEffect(() => {
+        setTimeout(()=>{
+            setDepartmentsData(fakeDepartmentsData);
+            if (Array.isArray(fakeDepartmentsData) && fakeDepartmentsData.length){
+                setEmployeesData(fakeDepartmentsData[0].employees)
+            }
+        }, 500)
+    }, []);
+
+    useEffect(() => {
+        const selectedDepartment = departmentsData.find(d => d.id === selectedDepartmentId); 
+        setEmployeesData(selectedDepartment ? selectedDepartment.employees : []); 
+        setSelectedEmployeeId(undefined);  
+    }, [departmentsData, selectedDepartmentId])
+
     useEffect (() => {
-        setLastName('');
-        setFirstName('');
-        setMidleName('');
+        clearEmployeeDialogFields();
         if (UserActionMode === "edit") {
             const employee = UserActionMode === 'edit'
                 ? employeesData.find(e => e.id === UserToEdit)
@@ -43,6 +65,12 @@ export const DepartmentsPage: FC = () => {
             setMidleName(employee?.middleName ?? '');
         }
     },[employeesData, UserActionMode, UserToEdit]);
+
+    const clearEmployeeDialogFields = () =>{
+        setLastName ('');
+        setFirstName('');
+        setMidleName('');
+    }
 
     const createEmployeeHandler = () => {
         setUserActionMode('create');
@@ -65,22 +93,44 @@ export const DepartmentsPage: FC = () => {
         )
     }
 
+    const closeEmployeeDialogHandler = () =>{
+        setShowEmployeeDialog(false);
+        clearEmployeeDialogFields();
+    }
+
+    const departmentChangedHandler = (id?: string) => {
+            const _id:  number | undefined = !id ? undefined: +id;
+            setSelectedDepartmentId(_id)
+    }
+
+    const onEmployeeSelectedHandler = (id: number) => {
+        setSelectedEmployeeId(id);
+    }
+
     return(
         <Layout>
+            <Dialog title={UserActionMode != 'edit' ? 'Добавить сотрудника' : 'Изменить сотрудника'}
+                open = {showEmployeeDialog}
+                onSave = {() => {}}
+                onCancel={closeEmployeeDialogHandler}
+            >
+                {userDialogContentRender()}
+            </Dialog>
+
+
             <div className="dep-page">
                 <div className="dep-page__users-list-container">
-                    <DropDown items={[{
-                            text: 'Отдел 1', value: '1'
-                        },{
-                            text: 'Отдел 2', value: '2'
-                        },{
-                            text: 'Отдел 3', value: '3'
-                        }]} 
+                    <DropDown items={departmentsData.map(dd => {
+                        return {
+                            text: dd.name,
+                            value: dd.id.toString(),
+                        } as DropDownItem;
+                    })} 
                         label = "Отделы:"
-                        selectedChanged = {(val) => console.log(val)}
+                        selectedChanged = {(val) => departmentChangedHandler(val)}
                     />
                     <EmployeesList employeesList={employeesData}
-                        onItemClick={(id) => console.log("selected", id)}
+                        onItemClick={(id) => onEmployeeSelectedHandler(id)}
                         onItemDelete={(id) => console.log("delete", id)}
                         onItemEdit={editEmployeeHandler}
                     />
@@ -91,7 +141,7 @@ export const DepartmentsPage: FC = () => {
                     <Dialog title = {UserActionMode !== 'edit' ? 'Добавить сотрудника' : 'Изменить сотрудника'}
                         open = {showEmployeeDialog}
                         onSave = {() => {}}
-                        onCancel= {() => setShowEmployeeDialog(false)}
+                        onCancel= {closeEmployeeDialogHandler}
                     >
                         {userDialogContentRender()}
                     </Dialog>
--- src/types/models.ts
+++ src/types/models.ts
@@ -1,6 +1,31 @@
+export interface Education {
+    id: number;
+    title: string;
+    description: string;
+}
+
+export interface WorkExperience {
+    id: number;
+    workedYears: number;
+    description?: string;
+}
+
 export interface Employee{
     id: number;
     firstName: string;
     lastName: string;
     middleName?: string;
+    email: string;
+    phoneNumber: string;
+    birthDate: string;
+    education: Array<Education>;
+    WorkExpirience: Array<WorkExperience>;
+
+}
+
+export interface Department{
+    id: number;
+    name: string;
+    descriprion?: string;
+    employees: Array<Employee>;
 }
